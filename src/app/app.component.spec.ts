1743c42fa651239418e1037d19af9e2e4f90e1f1
abeaucamp <beaucamp@proto-electronics.com>
2021-01-11 19:36:33 +0100

    Préparation de l'environement de dev

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('panoramax');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('panoramax app is running!');
-  });
 });

8e691cecee68ee1903c040f3651d1d5d4273624d
abeaucamp <beaucamp@proto-electronics.com>
2021-01-12 20:46:19 +0100

    Init Backend

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// TODO Connection db mongoose à mettre en place + fichier de config
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB okay !"))
+  .catch((error) => console.error(error));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+module.exports = app;
